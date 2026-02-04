6dc7ffbeb49b30a9572bce1201f7a488d4e9daac
ndionekhalifa78-hub <ndionekhalifa78@gmail.com>
2026-02-04 14:10:53 +0100

    Add files via upload

---
+++ portfolio.py
@@ -0,0 +1,54 @@
+import streamlit as st
+
+with st.sidebar:
+    st.markdown("""
+    Khalifa Ababacar NDIONE""")
+    st.markdown("""Géomaticien""")
+    st.markdown("""email:
+    ndionekhalifa78@gmail.com""")
+    st.markdown("""Téléphone:
+    776885051""")
+    st.markdown("""Adresse:
+    Dakar-HLM5""")
+    st.markdown("""Diplômes
+                
+    * BTS en Géomatique 
+    * Master en Géographie 
+    * Licence en Géographie
+    * Baccalauréat""")
+        
+
+st.markdown("""Ingénieur Géographe et Technicien supérieur en géomatique,je suis passionné par l'analyse, le traitement et la gestion des bases de données. Ainsi, grâce à mes connaissances et compétences, j'aimerais participer à la réalisation de projets et apporter des solutions face aux problèmes que rencontre le territoire.""")
+
+st.header("Compétences")
+st.markdown(""" 
+
+* Systèmes d'Information Géographique (SIG) : Maîtrise de logiciels comme QGIS, ArcGIS.
+* Gestion de bases de données : Conception, structuration, requêtage SQL, manipulation de données géographiques (raster/vecteur).
+* Analyse spatiale et statistique : Traitement de données pour modéliser des phénomènes, réaliser des analyses croisées et de l'analyse 3D.
+* Cartographie et visualisation : Production de cartes thématiques, représentations graphiques.
+* Acquisition de données : Topographie, GPS, numérisation, digitalisation, télédétection, imagerie satellite et drones.
+* Informatique et programmation : Langages de script (Python), langages web, automatisation de traitements. 
+* Réalisation de plan 2D et 3D sur Autocad et Sketchup
+* Analyse et synthèse : Comprendre les enjeux territoriaux et les traduire en données.""")
+
+st.header("Expériences")
+st.markdown("""
+* Digitalisation et correction topologique (batis, route, arbre) de notre quartier sur Qgis et Arcgis.
+
+* Réalisation de plan 2D(RDC, Etage, Terrasse, Masse, Coupe et Façade) sur Autocad et ensuite importation sur Sketchup pour la construction 3D)
+
+* Collecte de données (lampadaires, poteaux electriques, stations, gargottes, arbres, et panneaux publicitaires) de la commune de Fass-Colobane-Gueule tapée avec l'application Mobile Topographer puis traitement des données sur Arcgis 
+
+* Réalisation de carte de localisation et de carte thématique des types de sols d'une région du Sénégal
+
+* Création d'application web avec le module streamlit sur JupiterLab
+
+* Différentes tâche de codage avec le language python sur IDLE, jupiter notebook et sur jupiterlab
+
+* Création de base de données sur Qgis (géopackage et spatialite) et sur Arcgis (file géodatabase et personal géodatabase)
+""")
+
+
+
+
